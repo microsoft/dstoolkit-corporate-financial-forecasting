ac54c2afc5700d96635e7f0c77d8c809fd6bee5f
Mike Tokic <mitokic@microsoft.com>
2023-02-03 09:42:02 -0800

    package dep work

--- src/finn/download_dependencies.R
+++ src/finn/download_dependencies.R
@@ -14,17 +14,20 @@ options(
 )
 
 # download finnts dependencies
-download.packages(c('arrow', 'glmnet', 'kernlab'), 
+download.packages(c('arrow', 'shape', 'RcppEigen', 'glmnet', 'kernlab'), 
                   repos = 'https://packagemanager.rstudio.com/cran/__linux__/focal/2022-11-08', 
                   destdir = dest_dir)
 
-download.packages(c('clock', 'Cubist', 'hts', 'V8', 'parsnip', 'plotrix',
+download.packages(c('clock', 'Cubist', 'fracdiff', 'lmtest', 'quadprog', 'tseries', 'urca', 'RcppArmadillo', 
+                    'forecast', 'SparseM', 'hts', 'V8', 'parsnip', 'plotrix',
+                    'dygraphs', 'extraDistr', 'RcppParallel', 'rstan', 'rstantools', 'BH',
                     'Formula', 'plotmo', 'TeachingDemos', 'timechange', 'DiceDesign', 
                     'earth', 'dials', "tidyselect", "tidyr", "stringr", "rsample", 
                     'modeldata', 'rules', 'recipes', 'purrr', 'plyr', 'magrittr', 
                     'lubridate', 'bitops', 'snakecase', 'reactR', 'lhs', 'GPfit', 'tictoc', 
-                    'yardstick', 'reactable', 'gt', 'janitor', 'xgboost', 'gtools', 'fs', 
+                    'yardstick', 'reactable', 'checkmate', 'gt', 'janitor', 'xgboost', 'gtools', 'fs', 
                     'generics', 'doParallel', 'foreach', 'digest', 'GPfit', 'lhs', 'tune', 
+                    'StanHeaders', 'timetk', 'prophet',
                     'modeltime', 'modeltime.gluonts', 'modeltime.resample', 'remotes'), 
                   repos = 'https://packagemanager.rstudio.com/cran/__linux__/focal/2022-04-08', 
                   destdir = dest_dir)
